a8f61974182dc7ae5cb5a4668dfab836e23aacc5
Mario Corchero <mariocj89@gmail.com>
2015-11-17 18:58:39 +0000

    Raise when using wrong paths for local Orgs/Repos
    A new exception hierarchy is created based from Exception to represent
    workspace specific exceptions

--- hubsync/workspace.py
+++ hubsync/workspace.py
@@ -7,6 +7,20 @@ import git
 LOG = logging.getLogger('hubsync.workspace')
 
 
+class LocalWorkspaceError(Exception):
+    """Base exception for errors related to the local file workspace"""
+
+
+class InvalidPath(LocalWorkspaceError):
+    """Raised when a repository/org is attempted to be read in a path where it
+    does not exist"""
+
+
+def get_sub_folders(path):
+    """Returns the name of all subfolders within a path"""
+    return next(os.walk(path))[1]
+
+
 class Workspace(object):
     """Represents the current workspace directory"""
     def __init__(self, path):
@@ -17,9 +31,13 @@ class Workspace(object):
         """Returns the organizations within the workspace"""
         try:
             return [Organization(subdir, self.path)
-                    for subdir in next(os.walk(self.path))[1]]
+                    for subdir in get_sub_folders(self.path)]
         except StopIteration:
-            return []
+            raise InvalidPath("Unable to search for orgs within {0.path}, "
+                              "is the path correct?".format(self))
+
+    def __repr__(self):
+        return "<{0.__class__.__name__} ({0.path})>".format(self)
 
 
 class Repo(object):
@@ -32,8 +50,12 @@ class Repo(object):
         :param name: name of the repository
         """
         self.name = name.rstrip('/')
-        self.path = base_path + '/' + name
-        self.git = git.Repo(self.path)
+        self.path = os.path.join(base_path, self.name)
+        try:
+            self.git = git.Repo(self.path)
+        except git.exc.InvalidGitRepositoryError:
+            raise InvalidPath("Git repo don't exists in path {}".format(
+                self.path))
 
     def __repr__(self):
         """repr for an Repo"""
@@ -54,7 +76,7 @@ class Organization(object):
         :param name: name of the organization
         """
         self.name = name.rstrip('/')
-        self.path = base_path + '/' + name
+        self.path = os.path.join(base_path, self.name)
 
     def __repr__(self):
         """repr for an Organization"""
@@ -64,7 +86,10 @@ class Organization(object):
     def repos(self):
         """All repos within the organization"""
         try:
+            # Note get_sub_folders use os.walk, which means that you should not
+            # change the current working dir whilst generating the list of repos
             return [Repo(subdir, self.path)
-                    for subdir in next(os.walk(self.path))[1]]
+                    for subdir in get_sub_folders(self.path)]
         except StopIteration:
-            return []
+            raise InvalidPath("Unable to search for repos within org {0.name}, "
+                              "is path {0.path} correct?".format(self))
