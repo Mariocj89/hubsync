5abfcfe6ea0b56f4966b77b0d8b04bd2fd00ea23
Mario Corchero <mariocj89@gmail.com>
2015-11-21 10:39:30 +0000

    Add workspace path option to the prompt and config file
    Closes #2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='hubsync',
     packages=['hubsync'],
-    version='0.1.4',
+    version='0.1.5',
     description='Get your github workspace synced!',
     author='Mario Corchero',
     author_email='mariocj89@gmail.com',
